ffc97fa87b468959955b2c8f85894836a3cfa355
King Kong <king-kong@gmail.com>
2022-09-22 11:00:29 -0700

    display for the 100 babies

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 /* Imports */
+import { getBabies, renderBaby } from './fetch-utils.js';
 
 /* Get DOM Elements */
+const babyList = document.getElementById('baby-list');
 
 /* State */
 
+let astrology = [];
+let babies = [];
+let error = null;
+let count = 0;
+
 /* Events */
-
+window.addEventListener('load', async () => {
+    findBabies();
+    console.log(babies);
+});
+async function findBabies(name, astrology) {
+    const response = await getBabies();
+    error = response.error;
+    babies = response.data;
+    if (!error) {
+        displayBabies();
+    }
+}
 /* Display Functions */
+function displayBabies() {
+    babyList.innerHTML = '';
 
+    for (const baby of babies) {
+        const babyEl = renderBaby(baby);
+        babyList.append(babyEl);
+    }
+}
 // (don't forget to call any display functions you want to run on page load!)
--- fetch-utils.js
+++ fetch-utils.js
@@ -4,3 +4,29 @@ const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getBabies(name, astrology) {
+    let query = client.from('babies').select('*').order('name').limit(100);
+
+    const response = await query;
+
+    return response;
+}
+
+export function renderBaby(baby) {
+    const li = document.createElement('li');
+    li.classList.add('card');
+
+    const img = document.createElement('img');
+    img.src = img.alt = baby.name;
+
+    const h2 = document.createElement('h2');
+    h2.textContent = baby.name;
+
+    const p = document.createElement('p');
+    p.textContent = baby.astrology;
+
+    li.append(h2, img, p);
+
+    return li;
+}
